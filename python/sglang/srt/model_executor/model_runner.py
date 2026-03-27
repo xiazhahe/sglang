e776c995c9dfda675f94badd43fe5ed5816432e9
xiazhahe <1479989397@qq.com>
2026-03-27 11:04:24 +0800

    fix conflict between empty_cache and use_mem_pool

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1108,6 +1108,8 @@ class ModelRunner(ModelRunnerKVCacheMixin):
                 self.remote_instance_transfer_engine_weight_info = (
                     self.loader.remote_instance_transfer_engine_weight_info
                 )
+        if _is_npu:
+            torch.npu.empty_cache()
         monkey_patch_vllm_parallel_state(reverse=True)
 
         # Publish metadata to ModelExpress if running as seed source
